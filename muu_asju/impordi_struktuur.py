dbc57d0ea45d67fa0892870c820d97ca29170af5
smokeytube2 <test106437@gmail.com>
2024-11-15 13:38:25 +0200

    Arendasin asju mis ei ole põhiprogrammis

--- muu_asju/impordi_struktuur.py
+++ muu_asju/impordi_struktuur.py
@@ -2,8 +2,9 @@
 
 import os
 from graphviz import Digraph
+import re
 
-flowchart = Digraph("Import flowchart", filename="import_flowchart", format="png")
+flowchart = Digraph("Import flowchart", filename="import_flowchart", directory=os.getcwd()+"\\muu_asju", format="png")
 
 rootdir = os.getcwd()+"\\ksk"
 
@@ -11,29 +12,37 @@ classes = {}
 search = []
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
-        if file[-3:] == ".py" and "temp" not in file:
+        if file[-3:] == ".py" and "temp" not in file and file != "__init__.py":
             classes[file] = os.path.join(subdir, file)
             flowchart.node(file[:-3])
             search.append(file[:-3])
 
 print(search)
 
-def create_chart(start):
+used_pairs = []
+
+def create_chart(start, trigger=True):
     next_search = []
+    if trigger:
+        next_search = search.copy()
+        trigger = False
     with open(start, 'r', encoding="UTF-8") as f:
         cur = start.split("\\")[-1][:-3]
         for line in f:
+            line = line
             if "import" in line:
                 for key in search:
                     if key in line:
-                        if cur == key:
+                        if cur == key or [cur, key] in used_pairs:
                             break
                         flowchart.edge(cur, key)
+                        used_pairs.append([cur, key])
+                        used_pairs.append([key, cur])
                         next_search.append(key)
     if next_search == []:
         return
     for x in next_search:
-        create_chart(classes[x+".py"])
+        create_chart(classes[x+".py"], trigger=trigger)
     
                         
 create_chart(classes["main.py"])
--- muu_asju/inheritance_struktuur.py
+++ muu_asju/inheritance_struktuur.py
@@ -2,21 +2,24 @@
 
 import os
 from graphviz import Digraph
+import re
 
 def rotate_list(lst, n):
     for i in range(n):
         lst.insert(0, lst.pop())
     return lst
 
-flowchart = Digraph("Inheritance flowchart", filename="inheritance_flowchart", format="png")
+flowchart = Digraph("Inheritance flowchart", filename="inheritance_flowchart", directory=os.getcwd()+"\\muu_asju", format="png")
 
 rootdir = os.getcwd()+"\\ksk"
+
+pattern = r'\(([A-Za-zÄÖÜÕäöüõ]+)\)'
 
 classes = {}
 search = []
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
-        if file[-3:] == ".py" and "temp" not in file:
+        if file[-3:] == ".py" and "temp" not in file and file != "__init__.py":
             classes[file] = os.path.join(subdir, file)
             flowchart.node(file[:-3])
             search.append(file[:-3])
@@ -32,17 +35,17 @@ for x in range(len(search)):
             cur = fn.split("\\")[-1][:-3]
             for line in f:
                 if "class" in line:
-                    for key in search:
-                        if key in line:
-                            if cur == key or [cur, key] in used_pairs:
-                                break
-                            flowchart.edge(cur, key)
-                            used_pairs.append([cur, key])
-                            used_pairs.append([key, cur])
-                            next_search.append(key)
+                    parent = re.findall(pattern, line)
+                    if parent == []:
+                        continue
+                    if cur == parent[0] or [cur, parent[0]] in used_pairs:
+                        break
+                    flowchart.edge(cur, parent[0])
+                    used_pairs.append([cur, parent[0]])
+                    used_pairs.append([parent[0], cur])
+                    next_search.append(parent[0])
 
     rotate_list(search, 1)
-    print(search)
                     
 
 
